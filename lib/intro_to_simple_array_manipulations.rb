8d905ee7e6c431ddaa47e33a6ded4d0484673fe3
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-01 09:35:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,11 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  array.using.flatten
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at(array, interger)
